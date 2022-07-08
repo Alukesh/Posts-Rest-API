e39ca28b9c2bd4abc7ca3a85a1dec55748e1c8eb
Alukesh <akenzhebaev422@gmail.com>
2022-07-08 16:30:59 +0600

    first get posts with title and authors name from api. second pure js pagination

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 const postsList = document.querySelector('.postList');
-
+const postsOnPage = 4;
+const pagination = document.querySelectorAll('#pagination li');
 let posts ;
 let authors;
+
+
 const getPosts = async () =>{
    await fetch('https://jsonplaceholder.typicode.com/posts').then(res => {
         return res.json()
@@ -10,24 +13,40 @@ const getPosts = async () =>{
         return res.json()
     }).then(data => authors = data);
 
+    await pagination.forEach(item => item.addEventListener('click', (e) =>{
+        const pageNum = e.target.innerHTML;
 
-const listParent = document.createElement('div');
-  await posts.map(post =>{
+        let start = pageNum * postsOnPage - pageNum;
+        let end = start + postsOnPage;
+        const postPage = posts.slice(start, end);
+        postsList.innerHTML = '';
+
+
+         postPage.map(post =>{
+            const div = document.createElement('div');
+            const title = document.createElement('h2');
+            title.textContent = post.title;
+            const author = document.createElement('h3');
+            author.textContent = authors.filter(user => user.id === post.userId)[0].name;
+            div.append(title, author);
+            postsList.append(div)
+        });
+    }) );
+
+    await posts.slice(0, 4).map(post =>{
       const div = document.createElement('div');
       const title = document.createElement('h2');
       title.textContent = post.title;
       const author = document.createElement('h3');
       author.textContent = authors.filter(user => user.id === post.userId)[0].name;
       div.append(title, author);
-      listParent.append(div);
-      // console.log(authors.filter(user =>{ return   user.id === post.userId}))
+      postsList.append(div)
+        // console.log(authors.filter(user =>{ return   user.id === post.userId}))
   });
-  postsList.append(listParent);
-
-
-
 
 };
 getPosts();
+
+
 
 
