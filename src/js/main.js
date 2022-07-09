549a185d48065075c66865f7e4e98c24080bb143
Alukesh <akenzhebaev422@gmail.com>
2022-07-09 16:59:33 +0600

    slightly better new func showPost

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,29 @@
 const postsList = document.querySelector('.postList');
 const postsOnPage = 4;
 const pagination = document.querySelector('#pagination');
-let posts ;
+const form = document.querySelector('.header-under__form');
+let search = '';
+let posts = [];
+let searchPosts = [];
+
 let authors;
+let showPosts = (arr) =>{
+    arr.map(post =>{
+        const a = document.createElement('a');
+        a.addEventListener( 'click', () => {
+            localStorage.setItem('authorId', post.userId);
+            localStorage.setItem('postId', post.id)
+        });
+        a.setAttribute('href', './post.html');
+        a.classList.add('postBlock');
+        a.innerHTML = `
+            <h2>${post.title}</h2>
+            <h3>${authors.filter(user => user.id === post.userId)[0].name}</h3>
+            `;
+
+        postsList.append(a)
+    });
+};
 
 
 const getPosts = async () =>{
@@ -14,61 +35,44 @@ const getPosts = async () =>{
     }).then(data => authors = data);
 
 
+    form.addEventListener('submit' , (e) =>{
+        e.preventDefault();
+        search = e.target[0].value.toLowerCase();
+        console.log(search);
+        searchPosts = posts.filter(item => item.title.startsWith(search));
+        console.log(searchPosts);
+        postsList.innerHTML = '';
+        showPosts(searchPosts)
+    });
+
+
+
      for (let i = 1; i <= posts.length / postsOnPage; i++){
          pagination.innerHTML += `
           <li class="pagination-btn">${i}</li>
          `;
-
-         const paginationBtn = document.querySelectorAll('li');
+         const paginationBtn = document.querySelectorAll('.pagination-btn');
          paginationBtn.forEach(item => item.addEventListener('click' , (e) =>{
-             paginationBtn.forEach(el => {
-                 el.classList.remove('active')
-             });
-         item.classList.add('active');
+             paginationBtn.forEach(el => el.classList.remove('active'));
+             item.classList.add('active');
 
-         const pageNum = e.target.innerHTML;
-         let start = (pageNum - 1) * postsOnPage;
-         let end = start + postsOnPage;
-         const postPage = posts.slice(start, end);
-         postsList.innerHTML = '';
+             postsList.innerHTML = '';
+             const pageNum = e.target.innerHTML;
+             let start = (pageNum - 1) * postsOnPage;
+             let end = start + postsOnPage;
+             const postPage = posts.slice(start, end);
 
-         postPage.map(post =>{
-             const a = document.createElement('a');
-             a.addEventListener( 'click', () => {
-                 localStorage.setItem('authorId', post.userId);
-                 localStorage.setItem('postId', post.id)
-             });
-             a.setAttribute('href', './post.html');
-             a.classList.add('postBlock');
-             a.innerHTML = `
-            <h2>${post.title}</h2>
-            <h3>${authors.filter(user => user.id === post.userId)[0].name}</h3>
-            `;
+         showPosts(postPage);
 
-             postsList.append(a)
-         });
         } ))
 
 
 
 
     }
+    form[0].value= 'q';
 
-
-     posts.slice(0, postsOnPage).map(post =>{
-        let a = document.createElement('a');
-        a.innerHTML = `
-        <h2>${post.title}</h2>
-        <h3>${authors.filter(user => user.id === post.userId)[0].name}</h3>
-        `;
-        a.addEventListener( 'click', () => {
-            localStorage.setItem('authorId', post.userId);
-            localStorage.setItem('postId', post.id)
-        });
-        a.setAttribute('href', './post.html');
-        a.classList.add('postBlock');
-        postsList.append(a)
-
+     showPosts(posts.slice(0, postsOnPage))
       // const title = document.createElement('h2');
       // title.textContent = post.title;
       // const author = document.createElement('h3');
@@ -77,7 +81,6 @@ const getPosts = async () =>{
       // postsList.append(a)
 
         // console.log(authors.filter(user =>{ return   user.id === post.userId}))
-  });
 
 };
 getPosts();
